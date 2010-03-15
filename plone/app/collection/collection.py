8dc3898937b3fe7b306a0f48ffb497d13098a036
Ralph Jacobs <ralph@fourdigits.nl>
2010-03-15 13:55:00 +0000

    code cleanup and comments
    svn path=/plone.app.collection/trunk/; revision=34773

--- plone/app/collection/collection.py
+++ plone/app/collection/collection.py
@@ -1,34 +1,30 @@
 """Definition of the Collection content type
 """
 
-from zope.interface import implements, directlyProvides
-
+from zope.interface import implements
 from Products.Archetypes import atapi
 from Products.ATContentTypes.content import document
 from Products.ATContentTypes.content import schemata
-
 from plone.app.collection.interfaces import ICollection
 from plone.app.collection.config import PROJECTNAME
 
 CollectionSchema = document.ATDocumentSchema.copy() + atapi.Schema((
 
-    # -*- Your Archetypes field definitions here ... -*-
 ))
 
 # Set storage on fields copied from ATDocumentSchema, making sure
 # they work well with the python bridge properties.
-
 CollectionSchema['title'].storage = atapi.AnnotationStorage()
 CollectionSchema['description'].storage = atapi.AnnotationStorage()
 
 schemata.finalizeATCTSchema(
     CollectionSchema,
     folderish=False,
-    moveDiscussion=False
-)
+    moveDiscussion=False)
+
 
 class Collection(document.ATDocument):
-    """A Plone collection"""
+    """A Plone Collection"""
     implements(ICollection)
 
     meta_type = "Collection"
@@ -36,20 +32,19 @@ class Collection(document.ATDocument):
 
     title = atapi.ATFieldProperty('title')
     description = atapi.ATFieldProperty('description')
-    
+
     # -*- Your ATSchema to Python Property Bridges Here ... -*-
-    
     def getQueryDict(self):
         """Get the query dict from the request or from the object"""
         # Try the request to be current
-        query=getattr(self.REQUEST,'query', None)
+        query = getattr(self.REQUEST, 'query', None)
         if query:
             return query
         # Try the stored dict
-        query=getattr(self,'querydict', None)
+        query = getattr(self, 'querydict', None)
         if query:
             return query
-        
+
         # Nothing here. Should I raise an exception?
         return {}
 
