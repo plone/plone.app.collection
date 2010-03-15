9c38afca76259b3124b512f03a4ff1b6ef39e214
Maarten Kling <maarten@fourdigits.nl>
2010-03-15 16:31:26 +0000

    - move edit template to widget template
    svn path=/plone.app.collection/trunk/; revision=34831

--- plone/app/collection/browser/querybuilder.py
+++ plone/app/collection/browser/querybuilder.py
@@ -1,4 +1,3 @@
-from Products.Five.browser import BrowserView
 from plone.app.contentlisting.interfaces import IContentListing
 from Products.CMFCore.utils import getToolByName
 from config import CRITERION, SORTABLES
@@ -7,30 +6,26 @@ from plone.app.collection.queryparser import QueryParser
 from zope.component import queryMultiAdapter, getMultiAdapter
 from ZTUtils import make_query
 
-class QueryBuilder(BrowserView):
+class QueryBuilder(object):
     """ """
 
     # This is the advanced search that uses the query view from new-style-collections.
-    # If we end up not using this view for advanced search, it should probably be moved to the collections 
+    # If we end up not using this view for advanced search, it should probably be moved to the collections
     # package
+    _results = None
 
-    def __init__(self, context, request):
-        self._results = None
-        self.context = context
-        self.request = request
+    def getNumberOfResults(self, request):
+        return len(self.results(request))
 
-    def getNumberOfResults(self):
-        return len(self.results())
+    def getFormattedNumberOfResults(self, request):
+        return "%d items remaining" % (len(self.results(request)))
 
-    def getFormattedNumberOfResults(self):
-        return "%d items remaining" % (len(self.results()))
-
-    def results(self):
+    def results(self, request):
         if self._results is None:
-            self._results = self._queryForResults()
+            self._results = self._queryForResults(request)
         return self._results
 
-    def _queryForResults(self, formquery=None):
+    def _queryForResults(self, request, formquery=None):
         # parse query
         if not formquery:
             formquery=self.request.get('query', None)
@@ -52,17 +47,14 @@ class QueryBuilder(BrowserView):
             return IContentListing(results)
         return IContentListing([])
 
-    def printQuery(self):
-        return self.query
-
     def getConfig(self):
         return {'indexes':CRITERION, 'sortable_indexes': SORTABLES}
-        # we wrap this in a dictionary so we can add more configuration data 
-        # to the payload in the future. This is data that will be fetched 
+        # we wrap this in a dictionary so we can add more configuration data
+        # to the payload in the future. This is data that will be fetched
         # by a browser AJAX call
 
     def getJSONConfig(self):
         return json.dumps(self.getConfig())
 
-    def previewSearchResults(self):
-        return getMultiAdapter((self.context, self.request),name='querybuilderpreviewresults')()
+    def previewSearchResults(self, request, context):
+        return getMultiAdapter((context, request),name='querybuilderpreviewresults')()
