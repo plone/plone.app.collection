5003100422432b236a2ada4a53ddbea9d4cf8616
Roel Bruggink <roel+plone.org@fourdigits.nl>
2010-03-16 15:26:35 +0000

    Cleanup code
    svn path=/plone.app.collection/trunk/; revision=34973

--- plone/app/collection/queryparser.py
+++ plone/app/collection/queryparser.py
@@ -9,88 +9,93 @@ from zope.component import getUtility
 
 Row = namedtuple('Row', ['index', 'operator', 'values'])
 
+
 class QueryParser(object):
 
-    
     def __init__(self, context, request):
         self.context = context
         self.request = request
-    
+
     def parseFormquery(self, formquery):
         if not formquery:
             return {}
         reg = getUtility(IRegistry)
-        
+
         formquery = deepcopy(formquery)
         query = {}
         for row in formquery:
-            
+
             operator = row.get('o', None)
             function_path = reg["%s.operation" % operator]
-            
+
             # The functions expect this pattern of object, so lets give it to
             # them in a named tuple instead of jamming things onto the request
-            row = Row(index=row.get('i', None), 
-                      operator=function_path, 
+            row = Row(index=row.get('i', None),
+                      operator=function_path,
                       values=row.get('v', None))
-            
+
             kwargs = {}
-            
+
             module, function = row.operator.split(":")
             fromlist = module.split(".")[:-1]
             try:
                 module = __import__(module, fromlist=fromlist)
                 parser = getattr(module, function)
             except (ImportError, AttributeError):
-                raise # XXX: Be more friendly
+                raise  # XXX: Be more friendly
             else:
                 kwargs = parser(self.context, row)
 
             query.update(kwargs)
-        
+
         if not query:
             # If the query is empty fall back onto the equality query
             query = _equal(self.context, row)
-        
+
         return query
+
 
 # operators
 def _equal(context, row):
-    return {row.index: {
-                'query':row.values
-                }
-             }
+    return {row.index: {'query': row.values, }}
 
-# query.i:records=modified&query.o:records=between&query.v:records:list=2009/08/12&query.v:records:list=2009/08/14
+
+# query.i:records=modified&query.o:records=between&query.v:records:list=
+# 2009/08/12&query.v:records:list=2009/08/14
 # v[0] >= x > v[1]
 def _between(context, row):
     tmp = {row.index: {
-              'query':row.values,
-              'range':'minmax',
-              },
-          }
-    return tmp
-        
-# query.i:records=modified&query.o:records=larger_then_or_equal&query.v:records=2009/08/12
-# x >= value
-def _largerThan(context, row):
-    tmp = {row.index: {
-              'query':row.values,
-              'range':'min',
+              'query': row.values,
+              'range': 'minmax',
               },
           }
     return tmp
 
-# query.i:records=modified&query.o:records=less_then&query.v:records=2009/08/14
-# x < value
-def _lessThan(context, row):
+
+# query.i:records=modified&query.o:records=larger_then_or_equal&
+# query.v:records=2009/08/12
+# x >= value
+def _largerThan(context, row):
     tmp = {row.index: {
-              'query':row.values,
-              'range':'max',
+              'query': row.values,
+              'range': 'min',
               },
           }
     return tmp
-    
+
+
+# # query.i:records=modified&query.o:records=less_then&
+# query.v:records=2009/08/14
+# x < value
+def _lessThan(context, row):
+    tmp = {row.index: {
+              'query': row.values,
+              'range': 'max',
+              },
+          }
+    return tmp
+
+
 # current user
 def _currentUser(context, row):
     tmp = {row.index: {
@@ -99,50 +104,56 @@ def _currentUser(context, row):
           }
     return tmp
 
-# query.i:records=modified&query.o:records=less_then_relative_date&query.v:records=-7
+
+# query.i:records=modified&query.o:records=less_then_relative_date&
+# query.v:records=-7
 def _lessThanRelativeDate(context, row):
     values = int(row.values)
 
     now = DateTime()
     my_date = now + values
-    
+
     my_date = my_date.earliestTime()
     row.values = my_date
     return _lessThan(context, row)
 
-# query.i:records=modified&query.o:records=more_then_relative_date&query.v:records=-2
+
+# query.i:records=modified&query.o:records=more_then_relative_date&
+# query.v:records=-2
 def _moreThanRelativeDate(context, row):
-    values=int(row.values)
+    values = int(row.values)
 
     now = DateTime()
     my_date = now + values
-    
+
     my_date = my_date.latestTime()
     row.values = my_date
-    return _largerThanOrEqual(context, row)
+    return _largerThan(context, row)
+
 
 # query.i:records=path&query.o:records=path&query.v:records=/search/news/
-# query.i:records=path&query.o:records=path&query.v:records=08fb402c83d0e68cf4d547ea79f7680c
+#
+# query.i:records=path&query.o:records=path&
+# query.v:records=08fb402c83d0e68cf4d547ea79f7680c
 def _path(context, row):
     values = row.values
-    
+
     # UID
     if not '/' in values:
         values = getPathByUID(context, values)
-    
-    tmp={row.index:{
-        'query':values
-    }}
-    
+
+    tmp = {row.index: {'query': values, }}
+
     depth = getattr(row, 'depth', None)
-    if depth is not None: # can be 0
+    if depth is not None:  # could be 0
         tmp[row.index]['depth'] = int(depth)
     return tmp
-    
+
+
 # query.i:records=path&query.o:records=relative_path&query.v:records=../../..
 def _relativePath(context, row):
     t = len([x for x in row.values.split('/') if x])
-    
+
     obj = context
     for x in xrange(t):
         obj = aq_parent(obj)
@@ -150,10 +161,11 @@ def _relativePath(context, row):
     if obj and hasattr(obj, 'getPhysicalPath'):
         row.values = '/'.join(obj.getPhysicalPath())
         return _path(context, row)
-    
+
     row.values = '/'.join(obj.getPhysicalPath())
     return _path(context, row)
-    
+
+
 # Helper methods
 def getCurrentUsername(context):
     mt = getToolByName(context, 'portal_membership')
@@ -162,6 +174,7 @@ def getCurrentUsername(context):
         return user.getUserName()
     return ''
 
+
 def getPathByUID(context, uid):
     """Returns the path of an object specified by UID"""
 
@@ -169,6 +182,6 @@ def getPathByUID(context, uid):
     obj = reference_tool.lookupObject(uid)
 
     if obj:
-    	return '/'.join(obj.absolute_url())
+        return '/'.join(obj.absolute_url())
 
     return ""
