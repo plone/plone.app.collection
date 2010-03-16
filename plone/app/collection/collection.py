3f0a2185c80b637d83139a3fa040021397b120cf
Ralph Jacobs <ralph@fourdigits.nl>
2010-03-16 12:45:09 +0000

    Added Limit Search results and Number of items to collection
    svn path=/plone.app.collection/trunk/; revision=34927

--- plone/app/collection/collection.py
+++ plone/app/collection/collection.py
@@ -4,11 +4,15 @@
 
 from zope.interface import implements
 from Products.Archetypes import atapi
+from Products.Archetypes.atapi import BooleanField
+from Products.Archetypes.atapi import BooleanWidget
+from Products.Archetypes.atapi import IntegerField
+from Products.Archetypes.atapi import IntegerWidget
 from Products.ATContentTypes.content import document
 from Products.ATContentTypes.content import schemata
 from plone.app.collection.interfaces import ICollection
 from plone.app.collection.config import PROJECTNAME
-
+from Products.ATContentTypes import ATCTMessageFactory as _
 from queryfield import QueryField
 from querywidget import QueryWidget
 
@@ -27,6 +31,29 @@ CollectionSchema = document.ATDocumentSchema.copy() + atapi.Schema((
             description="Query for the collection",
         ),
     ),
+    BooleanField(
+        name='limitNumber',
+        required=False,
+        mode="rw",
+        default=False,
+        widget=BooleanWidget(
+            label=_(u'label_limit_number',
+                    default=u'Limit Search Results'),
+            description=_(u'help_limit_number',
+                          default=u"If selected, only the 'Number of Items' "
+                                   "indicated below will be displayed.")
+        ),
+    ),
+    IntegerField(
+        name='itemCount',
+            required=False,
+            mode="rw",
+            default=0,
+            widget=IntegerWidget(
+                    label=_(u'label_item_count', default=u'Number of Items'),
+                    description=''
+                    ),
+             ),
 ))
 
 # Set storage on fields copied from ATDocumentSchema, making sure
