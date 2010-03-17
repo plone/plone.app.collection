1e7e99989deb068b6673ec2141634e8bfbd02619
Ralph Jacobs <ralph@fourdigits.nl>
2010-03-17 16:38:36 +0000

    Added validators.py
    svn path=/plone.app.collection/trunk/; revision=35157

---
+++ plone/app/collection/validators.py
@@ -0,0 +1,27 @@
+from zope.interface import implements
+from Products.validation.interfaces.IValidator import IValidator
+
+
+class NonJavascriptValidator:
+    implements(IValidator)
+
+    name = 'nonjavascriptvalidator'
+
+    def __init__( self, name, title='', description=''):
+        self.name = name
+        
+        
+    def __call__(self, value, instance, *args, **kwargs):
+        """
+            Validates the given value
+        """
+        # value  is only empty when not using javascript
+        if len(value) == 0:
+            return "."
+        return 1
+    
+validatorList = [
+    NonJavascriptValidator('javascriptDisabled', title='', description=''),
+    ]
+
+__all__ = ('validatorList', )
