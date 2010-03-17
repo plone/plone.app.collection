770511750abb3fd7ae24aa8cbb029f85aaa59ac9
Roel Bruggink <roel+plone.org@fourdigits.nl>
2010-03-17 12:36:45 +0000

    Cleanup code
    svn path=/plone.app.collection/trunk/; revision=35093

--- plone/app/collection/browser/querybuilder.py
+++ plone/app/collection/browser/querybuilder.py
@@ -1,31 +1,27 @@
 from Products.Five.browser import BrowserView
 from plone.app.contentlisting.interfaces import IContentListing
 from plone.registry.interfaces import IRegistry
-from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone.utils import log
-from config import CRITERION, SORTABLES
+#from Products.CMFCore.utils import getToolByName
+#from Products.CMFPlone.utils import log
+#from config import CRITERION, SORTABLES
 from plone.app.collection.queryparser import QueryParser
-from zope.component import queryMultiAdapter, getMultiAdapter
-from ZTUtils import make_query
+#from zope.component import queryMultiAdapter, getMultiAdapter
+from zope.component import getMultiAdapter
+#from ZTUtils import make_query
 from zope.component import getUtility
 from plone.app.collection.registryreader import CollectionRegistryReader
 import json
 
+
 class QueryBuilder(BrowserView):
     """ """
-
-    # This is the advanced search that uses the query view from new-style-collections.
-    # If we end up not using this view for advanced search, it should probably be moved to the collections
-    # package
 
     def __init__(self, context, request):
         self._results = None
         self.context = context
         self.request = request
 
-
-    def __call__(self,value=None):
-        """Call"""
+    def __call__(self, value=None):
         if value is not None:
             self.value = value
         return self.index()
@@ -36,14 +32,14 @@ class QueryBuilder(BrowserView):
         return self._results
 
     def _queryForResults(self, formquery=None):
-        
+
         if formquery is None:
             if 'query' in self.request.form:
                 formquery = self.request.form.get('query', None)
             else:
                 formquery = self.value
-        
-        queryparser=QueryParser(self.context, self.request)
+
+        queryparser = QueryParser(self.context, self.request)
         query = queryparser.parseFormquery(formquery)
         if not query:
             return IContentListing([])
@@ -55,7 +51,6 @@ class QueryBuilder(BrowserView):
         #fetch and return the actual resultset
         return getMultiAdapter((self.context, self.request), name='searchResults')(query=query)
 
-
     def getNumberOfResults(self):
         return len(self.results())
 
@@ -63,7 +58,7 @@ class QueryBuilder(BrowserView):
         return "%d items remaining" % (len(self.results()))
 
     def getJSONConfig(self):
-        return json.dumps(self.getConfig())
+        return json.dumps(self.getConfigFromRegistry())
 
     def getConfigFromRegistry(self):
         """Returns the indexes and sortable indexes from the portal registry"""
@@ -77,4 +72,4 @@ class QueryBuilder(BrowserView):
         return json.dumps(self.getConfigFromRegistry())
 
     def previewSearchResults(self):
-        return getMultiAdapter((self.context, self.request),name='querybuilderpreviewresults')()
+        return getMultiAdapter((self.context, self.request), name='querybuilderpreviewresults')()
