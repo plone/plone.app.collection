e2d05a97335855a6232c1bad94e2f32960e755cb
Roel Bruggink <roel+plone.org@fourdigits.nl>
2010-03-17 14:15:09 +0000

    Use static config for now
    svn path=/plone.app.collection/trunk/; revision=35116

--- plone/app/collection/browser/querybuilder.py
+++ plone/app/collection/browser/querybuilder.py
@@ -3,7 +3,9 @@ from plone.app.contentlisting.interfaces import IContentListing
 from plone.registry.interfaces import IRegistry
 #from Products.CMFCore.utils import getToolByName
 #from Products.CMFPlone.utils import log
-#from config import CRITERION, SORTABLES
+from config import CRITERION, SORTABLES
+#from config import SORTABLES
+#import config
 from plone.app.collection.queryparser import QueryParser
 #from zope.component import queryMultiAdapter, getMultiAdapter
 from zope.component import getMultiAdapter
@@ -13,7 +15,6 @@ from plone.app.collection.interfaces import ICollectionRegistryReader
 import json
 
 
-
 class ContentListingView(BrowserView):
 
     def __init__(self, context, request):
@@ -21,8 +22,8 @@ class ContentListingView(BrowserView):
         self.request = request
 
     def __call__(self):
-        """Call"""
         return self.index()
+
 
 class QueryBuilder(BrowserView):
     """ """
@@ -41,10 +42,10 @@ class QueryBuilder(BrowserView):
         if self._results is None:
             self._results = self._queryForResults(formquery=formquery)
         return self._results
-    
+
     def html_results(self):
         return getMultiAdapter((self.results(), self.request), name='display_query_results')()
-    
+
     def _queryForResults(self, formquery=None):
 
         if formquery is None:
@@ -71,14 +72,14 @@ class QueryBuilder(BrowserView):
     def getFormattedNumberOfResults(self):
         return "%d items remaining" % (len(self.results()))
 
-    def getConfig(self): 
-        return {'indexes':CRITERION, 'sortable_indexes': SORTABLES} 
-        # we wrap this in a dictionary so we can add more configuration data 
-        # to the payload in the future. This is data that will be fetched 
-        # by a browser AJAX call 
+    def getConfig(self):
+        return {'indexes': CRITERION, 'sortable_indexes': SORTABLES}
+        #tmp = self.getConfigFromRegistry()
+        #tmp['sortable_indexes'] = config.SORTABLES
+        #return tmp
 
     def getJSONConfig(self):
-        return json.dumps(self.getConfigFromRegistry())
+        return json.dumps(self.getConfig())
 
     def getConfigFromRegistry(self):
         """Returns the indexes and sortable indexes from the portal registry"""
