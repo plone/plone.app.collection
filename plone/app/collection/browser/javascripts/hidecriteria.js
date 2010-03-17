9e703085e1a109e3662217aa028dfc182595da52
Rob Gietema <rob@fourdigits.nl>
2010-03-17 10:19:37 +0000

    Removed unused js file.
    svn path=/plone.app.collection/trunk/; revision=35075

--- plone/app/collection/browser/javascripts/hidecriteria.js
+++
@@ -1,6 +0,0 @@
-jQuery(function($) {
-    // Hide criteria tab for javascript enabled browsers
-    $(document).ready(function () {
-//        $('#contentview-criteria').hide();
-    });
-}(jQuery))
