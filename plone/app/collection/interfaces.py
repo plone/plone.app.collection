368674a7e875939f5039249b132b762446d3bb1e
Matthew Wilkes <matthew@matthewwilkes.co.uk>
2010-03-18 09:55:19 +0000

    Use the standard, non-persistent fields in the interface.  This prevents ZODB connection state being held by the interface
    svn path=/plone.app.collection/branches/matthewwilkes-fuckedtests/; revision=35240

--- plone/app/collection/interfaces.py
+++ plone/app/collection/interfaces.py
@@ -1,5 +1,5 @@
 from zope.interface import Interface
-from plone.registry import field
+from zope.schema import TextLine, Text, Bool, List, DottedName
 
 class ICollection(Interface):
     """ """
@@ -9,16 +9,16 @@ class ICollectionRegistryReader(Interface):
     """Marker interface for the registry adapter"""
 
 class IQueryOperation(Interface):
-    title = field.TextLine(title=u"Title")
-    description = field.Text(title=u"Description")
-    operation = field.TextLine(title=u"Operation")
-    widget = field.TextLine(title=u"Widget")
+    title = TextLine(title=u"Title")
+    description = Text(title=u"Description")
+    operation = TextLine(title=u"Operation")
+    widget = TextLine(title=u"Widget")
 
 class IQueryField(Interface):
-    title = field.TextLine(title=u"Title")
-    description = field.Text(title=u"Description")
-    enabled = field.Bool(title=u"Enabled")
-    sortable = field.Bool(title=u"Sortable")
-    operations = field.List(title=u"Operations", value_type=field.DottedName(title=u"Operation ID"))
-    vocabulary = field.TextLine(title=u"Vocabulary")
-    group = field.TextLine(title=u"Group")
+    title = TextLine(title=u"Title")
+    description = Text(title=u"Description")
+    enabled = Bool(title=u"Enabled")
+    sortable = Bool(title=u"Sortable")
+    operations = List(title=u"Operations", value_type=DottedName(title=u"Operation ID"))
+    vocabulary = TextLine(title=u"Vocabulary")
+    group = TextLine(title=u"Group")
