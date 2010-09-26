78f074d53da1cf3bff522fd95cd615ab2954b199
Roel Bruggink <roel+plone.org@fourdigits.nl>
2010-09-26 19:01:10 +0000

    Add tumbnail view
    svn path=/plone.app.collection/trunk/; revision=40283

--- plone/app/collection/collection.py
+++ plone/app/collection/collection.py
@@ -22,6 +22,8 @@ from plone.app.collection.interfaces import ICollection
 from plone.app.collection.config import PROJECTNAME, ATCT_TOOLNAME
 from plone.app.collection import PloneMessageFactory as _
 
+from plone.app.contentlisting.interfaces import IContentListing
+
 from archetypes.querywidget.field import QueryField
 from archetypes.querywidget.widget import QueryWidget
 
@@ -120,5 +122,22 @@ class Collection(document.ATDocument):
         for field in self.listMetaDataFields().items():
             _mapping[field[0]] = field
         return [_mapping[field] for field in self.customViewFields]
+
+    # Helper functions
+    def getFoldersAndImages(self):
+        catalog = getToolByName(self, 'portal_catalog')
+        folders = [item for item in self.results() if item.Type() == 'Folder']
+        _mapping = {'folders': folders,
+                    'images': {}}
+
+        for folder in folders:
+            query = {
+                'portal_type': 'Image',
+                'path': folder.getPath(),
+            }
+            _mapping['images'][folder.id] = IContentListing(catalog(query))
+
+        _mapping['total_number_of_images'] = sum(map(len, _mapping['images'].values()))
+        return _mapping
 
 atapi.registerType(Collection, PROJECTNAME)
