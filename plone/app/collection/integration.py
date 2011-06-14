d124b4adfc95c8368ba6315d2b254c326d32fcb9
Ralph Jacobs <ralph@fourdigits.nl>
2011-06-14 11:53:34 +0000

    Hide plone.app.collection dependency profiles from quickinstaller
    svn path=/plone.app.collection/trunk/; revision=50430

---
+++ plone/app/collection/integration.py
@@ -0,0 +1,31 @@
+"""
+    Miscellaneous things to help integrate new style collections into Plone.
+"""
+from zope.interface import implements
+from Products.CMFPlone.interfaces import INonInstallable \
+as IPloneFactoryNonInstallable
+from Products.CMFQuickInstallerTool.interfaces import INonInstallable \
+as IQuickInstallerNonInstallable
+
+
+class HiddenProfiles(object):
+    implements(IQuickInstallerNonInstallable, IPloneFactoryNonInstallable)
+
+    def getNonInstallableProfiles(self):
+        """
+        Prevents profiles dependencies from showing up in the profile list
+        when creating a Plone site.
+        """
+        return [u'archetypes.querywidget:default',
+                u'plone.app.querystring:default',
+                ]
+
+    def getNonInstallableProducts(self):
+        """
+        Prevents our dependencies from showing up in the quick
+        installer's list of installable products.
+        """
+        return [
+            'archetypes.querywidget',
+            'plone.app.querystring',
+            ]
