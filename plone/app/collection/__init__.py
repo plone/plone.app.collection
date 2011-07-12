7044407a5e1923d5983b07e00c39a8fef8375d17
Hanno Schlichting <hanno@hannosch.eu>
2011-07-12 17:21:20 +0000

    Cleanup __init__
    svn path=/plone.app.collection/trunk/; revision=51100

--- plone/app/collection/__init__.py
+++ plone/app/collection/__init__.py
@@ -1,6 +1,3 @@
-from plone.app.collection import config
-from Products.Archetypes import atapi
-from Products.CMFCore import utils
 from Products.validation.config import validation
 from zope.i18nmessageid import MessageFactory
 
@@ -10,32 +7,13 @@ PloneMessageFactory = MessageFactory('plone')
 
 
 def initialize(context):
-    """Initializer called when used as a Zope 2 product.
-
-    This is referenced from configure.zcml. Regstrations as a "Zope 2 product"
-    is necessary for GenericSetup profiles to work, for example.
-
-    Here, we call the Archetypes machinery to register our content types
-    with Zope and the CMF.
-    """
-
-    # Retrieve the content types that have been registered with Archetypes
-    # This happens when the content type is imported and the registerType()
-    # call in the content type's module is invoked. Actually, this happens
-    # during ZCML processing, but we do it here again to be explicit. Of
-    # course, even if we import the module several times, it is only run
-    # once.
+    from plone.app.collection import config
+    from Products.Archetypes import atapi
+    from Products.CMFCore import utils
 
     content_types, constructors, ftis = atapi.process_types(
         atapi.listTypes(config.PROJECTNAME),
         config.PROJECTNAME)
-
-    # Now initialize all these content types. The initialization process takes
-    # care of registering low-level Zope 2 factories, including the relevant
-    # add-permission. These are listed in config.py. We use different
-    # permissions for each content type to allow maximum flexibility of who
-    # can add which content types, where. The roles are set up in rolemap.xml
-    # in the GenericSetup profile.
 
     for atype, constructor in zip(content_types, constructors):
         utils.ContentInit('%s: %s' % (config.PROJECTNAME, atype.portal_type),
@@ -44,5 +22,6 @@ def initialize(context):
             extra_constructors=(constructor, ),
             ).initialize(context)
 
-# register validator
 validation.register(NonJavascriptValidator('javascriptDisabled'))
+
+__all__ = ('PloneMessageFactory', )
