6f8e9b31248a2881689a64e083e79e6d19946eef
Hanno Schlichting <hanno@hannosch.eu>
2011-07-12 17:36:35 +0000

    PEP8
    svn path=/plone.app.collection/trunk/; revision=51102

--- plone/app/collection/portlets/collectionportlet.py
+++ plone/app/collection/portlets/collectionportlet.py
@@ -1,55 +1,55 @@
-from zope.interface import implements
-from zope.component import getMultiAdapter
-from plone.portlets.interfaces import IPortletDataProvider
-from plone.app.portlets.portlets import base
-from zope import schema
-from zope.formlib import form
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.vocabularies.catalog import SearchableTextSourceBinder
 from plone.app.form.widgets.uberselectionwidget import UberSelectionWidget
-from plone.app.collection.interfaces import ICollection
+from plone.app.portlets.portlets import base
+from plone.app.vocabularies.catalog import SearchableTextSourceBinder
+from plone.portlets.interfaces import IPortletDataProvider
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import getMultiAdapter
+from zope.formlib import form
+from zope.interface import implements
+from zope import schema
+
 from plone.app.collection import PloneMessageFactory as _
 
 
 class ICollectionPortlet(IPortletDataProvider):
+    """A portlet which renders the results of a collection object.
     """
-        A portlet which renders the results of a collection object.
-    """
 
-    header = schema.TextLine(title=_(u"Portlet header"),
-                             description=_(u"Title of the rendered portlet"),
-                             required=True)
+    header = schema.TextLine(
+        title=_(u"Portlet header"),
+        description=_(u"Title of the rendered portlet"),
+        required=True)
 
-    target_collection = schema.Choice(title=_(u"Target collection"),
-                                  description=_(u"Find the collection which "
-                                                 "provides the items to list"),
-                                  required=True,
-                                  source=SearchableTextSourceBinder({'object_provides': ICollection.__identifier__},
-                                                                    default_query='path:'))
+    target_collection = schema.Choice(
+        title=_(u"Target collection"),
+        description=_(u"Find the collection which provides the items to "
+            "list"),
+        required=True,
+        source=SearchableTextSourceBinder({'portal_type': 'Collection'},
+            default_query='path:'))
 
-    limit = schema.Int(title=_(u"Limit"),
-                       description=_(u"Specify the maximum number of items to show in the portlet. "
-                                      "Leave this blank to show all items."),
-                       required=False)
+    limit = schema.Int(
+        title=_(u"Limit"),
+        description=_(u"Specify the maximum number of items to show in the "
+            "portlet. Leave this blank to show all items."),
+        required=False)
 
-    show_more = schema.Bool(title=_(u"Show more... link"),
-                       description=_(u"If enabled, a more... link will appear in the footer of the portlet, "
-                                      "linking to the underlying Collection."),
-                       required=True,
-                       default=True)
+    show_more = schema.Bool(
+        title=_(u"Show more... link"),
+        description=_(u"If enabled, a more... link will appear in the "
+            "footer of the portlet, linking to the underlying Collection."),
+        required=True,
+        default=True)
 
-    show_dates = schema.Bool(title=_(u"Show dates"),
-                       description=_(u"If enabled, effective dates will be shown underneath the items listed."),
-                       required=True,
-                       default=False)
+    show_dates = schema.Bool(
+        title=_(u"Show dates"),
+        description=_(u"If enabled, effective dates will be shown "
+            "underneath the items listed."),
+        required=True,
+        default=False)
 
 
 class Assignment(base.Assignment):
-    """
-        Portlet assignment.
-        This is what is actually managed through the portlets UI and associated
-        with columns.
-    """
 
     implements(ICollectionPortlet)
 
@@ -79,12 +79,6 @@ class Assignment(base.Assignment):
 
 
 class Renderer(base.Renderer):
-    """Portlet renderer.
-
-    This is registered in configure.zcml. The referenced page template is
-    rendered, and the implicit variable 'view' will refer to an instance
-    of this class. Other methods can be added and referenced in the template.
-    """
 
     _template = ViewPageTemplateFile('templates/collectionportlet.pt')
 
@@ -118,8 +112,7 @@ class Renderer(base.Renderer):
         return results
 
     def collection(self):
-        """ get the collection the portlet is pointing to"""
-
+        """Get the collection the portlet is pointing to"""
         collection_path = self.data.target_collection
         if not collection_path:
             return None
@@ -134,36 +127,29 @@ class Renderer(base.Renderer):
             name=u'plone_portal_state')
         portal = portal_state.portal()
         if isinstance(collection_path, unicode):
-            #restrictedTraverse accept only strings
+            # restrictedTraverse accept only strings
             collection_path = str(collection_path)
         return portal.restrictedTraverse(collection_path, default=None)
 
 
 class AddForm(base.AddForm):
-    """Portlet add form.
-        This is registered in configure.zcml. The form_fields variable tells
-        zope.formlib which fields to display. The create() method actually
-        constructs the assignment that is being added.
-    """
+
     form_fields = form.Fields(ICollectionPortlet)
     form_fields['target_collection'].custom_widget = UberSelectionWidget
 
     label = _(u"Add Collection Portlet")
-    description = _(u"This portlet display a listing of items from a Collection.")
+    description = _(u"This portlet display a listing of items from a "
+        "Collection.")
 
     def create(self, data):
         return Assignment(**data)
 
 
 class EditForm(base.EditForm):
-    """
-        Portlet edit form.
-        This is registered with configure.zcml. The form_fields variable tells
-        zope.formlib which fields to display.
-    """
 
     form_fields = form.Fields(ICollectionPortlet)
     form_fields['target_collection'].custom_widget = UberSelectionWidget
 
     label = _(u"Edit Collection Portlet")
-    description = _(u"This portlet display a listing of items from a Collection.")
+    description = _(u"This portlet display a listing of items from a "
+        "Collection.")
