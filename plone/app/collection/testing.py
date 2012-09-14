7eb05018a7224996330e649536dcaf62094d94d7
Timo Stollenwerk <contact@timostollenwerk.net>
2012-09-14 21:08:41 +0200

    Acceptance test for type criterion added.

--- plone/app/collection/testing.py
+++ plone/app/collection/testing.py
@@ -4,6 +4,9 @@ from zope.configuration import xmlconfig
 
 from plone.testing.z2 import ZSERVER_FIXTURE
 
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
+
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import applyProfile
 from plone.app.testing.layers import FunctionalTesting
@@ -19,6 +22,12 @@ class PloneAppCollectionLayer(PloneSandboxLayer):
 
     def setUpPloneSite(self, portal):
         applyProfile(portal, 'plone.app.collection:default')
+        setRoles(portal, TEST_USER_ID, ['Manager'])
+        portal.invokeFactory(
+            "Folder",
+            id="acceptance-test-folder",
+            title=u"Test Folder"
+        )
 
 
 PLONEAPPCOLLECTION_FIXTURE = PloneAppCollectionLayer()
