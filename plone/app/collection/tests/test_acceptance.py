88c14239671e041ea2a4f8d2cefae9287fa70109
Carsten Rebbien <cr@comlounge.net>
2012-11-07 17:57:13 +0100

    added robotframework tests according to master branch (dexterity version)

---
+++ plone/app/collection/tests/test_acceptance.py
@@ -0,0 +1,24 @@
+import unittest
+
+from plone.testing import layered
+
+from plone.app.collection.testing import PLONEAPPCOLLECTION_ACCEPTANCE_TESTING
+
+import robotsuite
+
+
+#def test_suite():
+#    suite = unittest.TestSuite()
+#    suite.addTests([
+#        layered(robotsuite.RobotTestSuite("acceptance/test_collection.txt"),
+#        layer=PLONEAPPCOLLECTION_ACCEPTANCE_TESTING),
+#    ])
+#    return suite
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTests([
+        layered(robotsuite.RobotTestSuite("acceptance/test_comlounge.txt"),
+        layer=PLONEAPPCOLLECTION_ACCEPTANCE_TESTING),
+    ])
+    return suite
