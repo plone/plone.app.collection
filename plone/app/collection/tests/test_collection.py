7b868376ed9e2085adeb98d912df277bf559e85f
Gil Forcada <gil.forcada@freitag.de>
2012-12-21 10:00:51 +0100

    Updated deprecated test methods

--- plone/app/collection/tests/test_collection.py
+++ plone/app/collection/tests/test_collection.py
@@ -56,13 +56,13 @@ class PloneAppCollectionClassTest(unittest.TestCase):
         self.collection = self.portal['collection']
 
     def test_listMetaDataFields(self):
-        self.assertEquals(self.collection.listMetaDataFields(), [])
+        self.assertEqual(self.collection.listMetaDataFields(), [])
 
     def test_results(self):
         pass
 
     def test_selectedViewFields(self):
-        self.assertEquals(self.collection.selectedViewFields(), [])
+        self.assertEqual(self.collection.selectedViewFields(), [])
 
     def test_getFoldersAndImages(self):
         pass
@@ -100,7 +100,7 @@ class PloneAppCollectionIntegrationTest(unittest.TestCase):
         fti = queryUtility(IDexterityFTI,
                            name='Collection')
         schema = fti.lookupSchema()
-        self.assertEquals(ICollection, schema)
+        self.assertEqual(ICollection, schema)
 
     def test_fti(self):
         fti = queryUtility(IDexterityFTI,
@@ -112,13 +112,13 @@ class PloneAppCollectionIntegrationTest(unittest.TestCase):
                            name='Collection')
         factory = fti.factory
         new_object = createObject(factory)
-        self.failUnless(ICollection.providedBy(new_object))
+        self.assertTrue(ICollection.providedBy(new_object))
 
     def test_adding(self):
         self.folder.invokeFactory('Collection',
                                   'collection1')
         p1 = self.folder['collection1']
-        self.failUnless(ICollection.providedBy(p1))
+        self.assertTrue(ICollection.providedBy(p1))
 
 
 class PloneAppCollectionViewsIntegrationTest(unittest.TestCase):
@@ -141,32 +141,32 @@ class PloneAppCollectionViewsIntegrationTest(unittest.TestCase):
     def test_view(self):
         view = self.collection.restrictedTraverse('@@view')
         self.assertTrue(view())
-        self.assertEquals(view.request.response.status, 200)
+        self.assertEqual(view.request.response.status, 200)
 
     def test_standard_view(self):
         view = self.collection.restrictedTraverse('standard_view')
         self.assertTrue(view())
-        self.assertEquals(view.request.response.status, 200)
+        self.assertEqual(view.request.response.status, 200)
 
     def test_summary_view(self):
         view = self.collection.restrictedTraverse('summary_view')
         self.assertTrue(view())
-        self.assertEquals(view.request.response.status, 200)
+        self.assertEqual(view.request.response.status, 200)
 
     def test_all_content(self):
         view = self.collection.restrictedTraverse('all_content')
         self.assertTrue(view())
-        self.assertEquals(view.request.response.status, 200)
+        self.assertEqual(view.request.response.status, 200)
 
 #    def test_tabular_view(self):
 #        view = self.collection.restrictedTraverse('tabular_view')
 #        self.assertTrue(view())
-#        self.assertEquals(view.request.response.status, 200)
+#        self.assertEqual(view.request.response.status, 200)
 
     def test_thumbnail_view(self):
         view = self.collection.restrictedTraverse('thumbnail_view')
         self.assertTrue(view())
-        self.assertEquals(view.request.response.status, 200)
+        self.assertEqual(view.request.response.status, 200)
 
 
 class PloneAppCollectionEditViewsIntegrationTest(unittest.TestCase):
@@ -194,7 +194,7 @@ class PloneAppCollectionEditViewsIntegrationTest(unittest.TestCase):
         #from plone.app.contentlisting.interfaces import IContentListing
         #self.assertTrue(IContentListing.providedBy(view.accessor()))
         #self.assertTrue(getattr(accessor(), "actual_result_count"))
-        #self.assertEquals(accessor().actual_result_count, 0)
+        #self.assertEqual(accessor().actual_result_count, 0)
 
     def test_collection_templates(self):
         portal = self.layer['portal']
