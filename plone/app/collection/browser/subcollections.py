a8839d9d822ae00e2dcebf511e75b82525d5242f
Maurits van Rees <maurits@vanrees.org>
2013-04-03 14:01:45 +0200

    Sub collections view: backwards compatibility for Plone 4.2.
    Or plone.app.content 2.0.

--- plone/app/collection/browser/subcollections.py
+++ plone/app/collection/browser/subcollections.py
@@ -1,7 +1,14 @@
 from Acquisition import aq_inner
 from plone.app.content.browser.foldercontents import FolderContentsView
 from plone.app.content.browser.foldercontents import FolderContentsTable
-from plone.app.content.browser.foldercontents import FolderContentsBrowserView
+try:
+    from plone.app.content.browser.foldercontents import \
+        FolderContentsBrowserView
+    FolderContentsBrowserView  # pyflakes
+except ImportError:
+    # BBB for Plone 4.2 (plone.app.content 2.0)
+    from plone.app.content.browser.foldercontents import \
+        FolderContentsKSSView as FolderContentsBrowserView
 
 
 class SubCollectionsView(FolderContentsView):
@@ -9,6 +16,15 @@ class SubCollectionsView(FolderContentsView):
     def contents_table(self):
         table = SubCollectionsTable(aq_inner(self.context), self.request)
         return table.render()
+
+    def renderBase(self):
+        """Returns the URL used in the base tag.
+
+        BBB for Plone 4.2 (plone.app.content 2.0).  Later versions of
+        plone.app.content define their own renderBase already.
+        """
+        # Assume a folderish context
+        return self.context.absolute_url() + '/'
 
     def add_url(self):
         return '%s/createObject?type_name=Collection' % self.renderBase()
